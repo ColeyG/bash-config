c43df629d5f5b567309e2b0c857e3d30d5d2a32d
ColeyG <cjgeerts@gmail.com>
2019-12-28 23:39:17 -0500

    Add Todo | Add quickCppCompile flags

--- quickCppCompile.sh
+++ quickCppCompile.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+r=0
+
+while getopts 'tr' opt; do
+    case $opt in
+      t) echo 'Null T Flag Set' ;;
+      r) r=1 ;;
+      *) echo 'Error in command line parsing' >&2
+         exit 1
+    esac
+  done
+
+shift "$(( OPTIND - 1 ))"
+
+
 file=$1
 output=$(echo ${file} | sed 's/.cpp//')
 
 g++ "$(pwd)/${file}" -o "$(pwd)/${output}"
 
+if [ "$r" -eq 1 ]; then
+  ./"${output}";
+fi
