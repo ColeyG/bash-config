fc22b67ce7d0971dbe991d078002c60a4d220fd1
ColeyG <cjgeerts@gmail.com>
2020-08-19 11:22:43 -0400

    nglog merge

--- aliases/misc.sh
+++ aliases/misc.sh
@@ -5,12 +5,9 @@ alias 'clip'='xsel -ib'
 alias 'ophere'='$(nautilus . &)'
 alias 'wo'='explorer.exe .'
 alias 'tab'='gnome-terminal --tab'
-<<<<<<< HEAD
 alias 'nglog'='cat /var/log/nginx/error.log'
-=======
 alias 'bctar'='tar -xvf'
 alias 'bctargz'='tar -zxvf'
->>>>>>> 367de15cdb7419090f108c2a86b858416c24d21e
 crtab() {
   google-chrome localhost/${PWD##*/};
 }
