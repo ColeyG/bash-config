0788bf8fc5364afa63b3e33cdb2d4212475cf114
ColeyG <cjgeerts@gmail.com>
2020-08-27 10:57:40 -0400

    Update Git Profiles

--- aliases/git.sh
+++ aliases/git.sh
@@ -2,6 +2,10 @@
 
 alias 'gst'='git status'
 alias 'grst'='git reset --hard;git clean -fd;git pull'
+alias 'gsee'='git log -10'
+grevert() {
+  git reset --hard "$@";
+}
 gcm() {
   git add .; git commit -m "$@";
 }
