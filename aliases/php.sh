5cc9289f4a5da9bde8ff0d18bf9a30761ab7f7c8
Cole Geerts <cjgeerts@gmail.com>
2022-11-15 12:29:20 -0500

    Remove PHP Aliases

--- aliases/php.sh
+++ aliases/php.sh
@@ -1,27 +1,6 @@
 #!/bin/bash
 
-alias 'pbm'='php bin/magento'
-alias 'pbmcf'='php bin/magento cache:flush'
-alias 'pbmsu'='php bin/magento setup:upgrade'
 alias 'phpunit'='vendor/phpunit/phpunit/phpunit'
-mg() {
-    php bin/magento "$@";
-}
-mg1() {
-    php7.1 bin/magento "$@";
-}
-mg2() {
-    php7.2 bin/magento "$@";
-}
-comp() {
-    php /usr/local/bin/composer "$@";
-}
-composer1() {
-    php7.1 /usr/local/bin/composer "$@";
-}
-composer2() {
-    php7.2 /usr/local/bin/composer "$@";
-}
 phpformat() {
     prettier "$@"*.php --write;
 }
