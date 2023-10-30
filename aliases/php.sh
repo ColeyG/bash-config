07ffa819ac348ede23ce9944b897a3c195691078
Cole Geerts <Cole.Geerts@lhsc.on.ca>
2023-10-30 11:22:24 -0400

    Adding Twig Alias

--- aliases/php.sh
+++ aliases/php.sh
@@ -4,3 +4,5 @@ alias 'phpunit'='vendor/phpunit/phpunit/phpunit'
 phpformat() {
     prettier "$@"*.php --write;
 }
+# Enable Twig
+alias 'twigdebug'='drush state:set twig_debug 1 --input-format=integer;drush state:set twig_cache_disable 1 --input-format=integer;drush state:set disable_rendered_output_cache_bins 1 --input-format=integer;drush cache:rebuild'
--- extra_aliases.sh
+++ extra_aliases.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-version=2.1.3
-date="oct-2-2023"
+version=2.1.4
+date="oct-30-2023"
 
 . ~/bash-config/aliases/git.sh
 . ~/bash-config/aliases/js.sh
